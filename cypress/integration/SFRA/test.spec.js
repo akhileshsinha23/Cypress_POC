cda890fd9c7f98a10494266cbed8b1992b32e5ab
Akhilesh <akksi@pandora.net>
2021-04-26 18:30:34 +0200

    first commit

---
+++ cypress/integration/SFRA/test.spec.js
@@ -0,0 +1,7 @@
+describe('My First Test', () => {
+    it('Visits the CI Sandbox', () => {
+    //   cy.visit('https://bfcr-001.sandbox.us01.dx.commercecloud.salesforce.com/')
+    //   cy.visit('https://www.google.com')
+    cy.visit('/')
+    })
+  })
